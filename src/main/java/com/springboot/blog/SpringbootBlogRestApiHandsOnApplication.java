71c27d8ae128f984617caec5a0a379a0986f3f2d
Camilo Barros <Camilo.Barros@Millicom.com>
2023-05-09 16:37:07 -0400

    Initial commit

---
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiHandsOnApplication.class, args);
+	}
+
+}
