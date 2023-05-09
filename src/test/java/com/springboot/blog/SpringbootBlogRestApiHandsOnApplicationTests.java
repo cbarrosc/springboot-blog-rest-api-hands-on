cafbcca65c63f691b3062b9b64e397c0ae0c6b0c
Camilo Barros <camilo.barros.c@gmail.com>
2023-05-09 17:17:30 -0400

    Include MySQL database connection settings in application.yml

--- src/test/java/com/springboot/blog/SpringbootBlogRestApiHandsOnApplicationTests.java
+++ src/test/java/com/springboot/blog/SpringbootBlogRestApiHandsOnApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootBlogRestApiHandsOnApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
